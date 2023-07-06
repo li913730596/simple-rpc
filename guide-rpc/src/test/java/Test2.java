76d0b246bd6eb5615569af2be1c3bc584bb52ad2
JarNinLee <bupt@edu.cn>
2023-07-06 11:48:06 +0800

    使用socket，jdk proxy, 服务注册, 初步简单实现一个rpc通信

--- guide-rpc/src/test/java/Test2.java
+++ guide-rpc/src/test/java/Test2.java
@@ -7,6 +7,6 @@ public class Test2 {
 
         RpcClientProxy proxy = new RpcClientProxy("127.0.0.1", 8080);
         HelloService clinet = proxy.getClinet(HelloService.class);
-        System.out.println(clinet.sayHello("李明"));
+        System.out.println(clinet.sayHello("李红"));
     }
 }
